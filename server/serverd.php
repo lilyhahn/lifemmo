b940d3a40b04cd9afbeffc915bc84464c4e73072
Tobi Hahn <cmpgk1024@gmail.com>
2014-03-29 16:31:29 -0400

    daemon

---
+++ server/serverd.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+require('LifeConsole.php');
+$rule = new Rule(array(2, 3), array(3));
+$serv = new LifeConsole(30, 30, $rule);
+function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
+$serv->start();
+?>
